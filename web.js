2cc0e9ed740ca3269ab76812ab29b444fca964ba
startup <startup@startup.(none)>
2013-07-09 22:36:37 +0200

    Web.js changed to index

--- web.js
+++ web.js
@@ -2,11 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require("fs");
+var content=fs.readFileSync("index.html");
+var buffer = new Buffer(content);
+var content2=buffer.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content2);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
